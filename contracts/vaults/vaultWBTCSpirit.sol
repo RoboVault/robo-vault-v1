3714138759b1d3e52ae2b4c95bf865d0802dc609
SmoothBot <SmoothBot>
2021-07-11 22:41:41 +1000

    misc

--- contracts/vaults/vaultWBTCSpirit.sol
+++ contracts/vaults/vaultWBTCSpirit.sol
@@ -6,8 +6,8 @@ import "../token.sol";
 
 contract rbUSDCSpirit is ERC20, ERC20Detailed, Token {
     using SafeMath for uint256;
-    IFarm farm = new Spirit();
-    ILend lend = new Cream();
+    Spirit farm = new Spirit();
+    Cream lend = new Cream();
     address constant WBTC = 0x321162Cd933E2Be498Cd2267a90534A804051b11;
     address constant WFTM = 0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83;
 
